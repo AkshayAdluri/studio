cabd4d90a1e206558c9818f201983ec567edcd14
akshay adluri <akshayadluri@gmail.com>
2025-07-17 16:20:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -64,7 +64,7 @@ export function SearchBar() {
 
     const debounceTimeout = setTimeout(fetchSuggestions, 300);
     return () => clearTimeout(debounceTimeout);
-  }, [searchTerm, categories]);
+  }, [searchTerm]);
 
   return (
     <div className="relative w-full" ref={searchContainerRef}>
